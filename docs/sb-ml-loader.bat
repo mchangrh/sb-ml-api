b1a2caf3cfc254d6463ee6aae5a09fc735706aa7
Michael C <michael@mchang.name>
2022-03-11 00:51:05 -0500

    prefix path

--- docs/sb-ml-loader.bat
+++ docs/sb-ml-loader.bat
@@ -1,4 +1,4 @@
 @echo off
 set "auth=test"
-curl --data-binary "@%1" -H "content-type: text/plain" https://sb-ml.mchang.xyz/load?auth=%auth%
+curl --data-binary "@%1" -H "content-type: text/plain" https://sb-ml.mchang.xyz/ml/load?auth=%auth%
 pause
