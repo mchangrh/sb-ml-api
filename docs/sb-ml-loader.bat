d9749ebe7e61960d92fb39a4c85665cfa77607e2
Michael C <michael@mchang.name>
2022-04-19 18:52:02 -0400

    update subdomain

--- docs/sb-ml-loader.bat
+++ docs/sb-ml-loader.bat
@@ -1,4 +1,4 @@
 @echo off
 set "auth=test"
-curl --data-binary "@%1" -H "content-type: text/plain" https://sb-ml.mchang.xyz/ml/load?auth=%auth%
+curl --data-binary "@%1" -H "content-type: text/plain" https://ml.sb.mchang.xyz/ml/load?auth=%auth%
 pause
