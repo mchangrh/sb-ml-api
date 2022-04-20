ff0078a1e19ad3b4f404bf9279486fb9a2118ebe
Michael C <michael@mchang.name>
2022-04-20 16:45:29 -0400

    update endpoint to use /load

--- docs/sb-ml-loader.sh
+++ docs/sb-ml-loader.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 upload () {
   AUTH=test
-  curl --data-binary "@$1" -H "content-type: text/plain" https://ml.sb.mchang.xyz/ml/load?auth=$AUTH
+  curl --data-binary "@$1" -H "content-type: text/plain" https://ml.sb.mchang.xyz/load?auth=$AUTH
 }
